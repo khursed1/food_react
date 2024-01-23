27478f0dff61425bbd1e915f19e38574eb9bcc49
Ramkrishn <ram706860@gmail.com>
2024-01-23 16:37:01 +0530

    issue resolved

--- src/components/ResturantCatogry.jsx
+++ src/components/ResturantCatogry.jsx
@@ -1,28 +1,32 @@
 import ItemList from "./ItemList";
 import { useState } from "react";
-const ResturantCatogry = ({ data, showitems, setResOpen }) => {
-const [isOpen, setIsOpen] = useState(false);
-    const { title } = data;
-    const handleClick = () => {
-        isOpen ? setResOpen(null) : setResOpen();
-        setIsOpen(true);
-        console.log(isOpen)
+const ResturantCatogry = ({ data, showitems, setResOpen, index }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { title } = data;
+  const handleClick = () => {
+    if (!showitems) {
+      setIsOpen(!isOpen);
+      setResOpen(index);
+      return;
     }
-    return (
-        <div>
-
-            <div className="bg-gray-50 w-6/12 mx-auto my-5 shadow-lg p-4">
-                <div className="flex justify-between cursor-pointer" onClick={
-                    handleClick
-
-
-                }>
-                    <span className="font-medium">{title}({data.itemCards.length})</span>
-                    {showitems ? (<span>⬆️</span>) : (<span>⬇️</span>)}
-                </div>
-                {showitems && (<ItemList items={data.itemCards} />)}
-            </div>
+    !isOpen ? setResOpen(null) : setResOpen(index);
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <div className="bg-gray-50 w-6/12 mx-auto my-5 shadow-lg p-4">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="font-medium">
+            {title}({data.itemCards.length})
+          </span>
+          {showitems ? <span>⬆️</span> : <span>⬇️</span>}
         </div>
-    )
-}
+        {showitems && <ItemList items={data.itemCards} />}
+      </div>
+    </div>
+  );
+};
 export default ResturantCatogry;
--- src/components/ResturantMenu.jsx
+++ src/components/ResturantMenu.jsx
@@ -1,37 +1,42 @@
 import { useParams } from "react-router-dom";
-import useResturantMenu from "../utils/useFetchMenu"
+import useResturantMenu from "../utils/useFetchMenu";
 import ResturantCatogry from "./ResturantCatogry";
 import { useState } from "react";
 const ResturantMenu = () => {
-    const [resOpen, setResOpen] = useState(0);
-    let key = 1;
-    const { resID } = useParams();
-    const resMenu = useResturantMenu(resID);
-    if (!resMenu) {
-        // Render loading state or return null
-        return <p>Loading...</p>;
-    }
+  const [resOpen, setResOpen] = useState(0);
+  let key = 1;
+  const { resID } = useParams();
+  const resMenu = useResturantMenu(resID);
+  if (!resMenu) {
+    // Render loading state or return null
+    return <p>Loading...</p>;
+  }
 
-    const { name, cuisines, costForTwoMessage, } = resMenu.data.cards[0]?.card?.card?.info;
-    const catogries = resMenu?.data?.cards[2]?.groupedCard
-        ?.cardGroupMap?.REGULAR.cards.filter(c => c.card?.card?.["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
-    return (
-        <div className="text-center">
-            <h1 className="font-semibold m-3 text-2xl">{name}</h1>
-            <p className="font-semibold text-lg">
-                {cuisines.join(', ')}:-
-
-                {costForTwoMessage}
-
-            </p>
-            {catogries.map((catogry, index) => (
-                <ResturantCatogry
-                    key={++key}
-                    data={catogry?.card?.card}
-                    showitems={index == resOpen ? true : false}
-                    setResOpen={() => setResOpen(index)} />
-            ))}
-        </div>
-    )
-}
+  const { name, cuisines, costForTwoMessage } =
+    resMenu.data.cards[0]?.card?.card?.info;
+  const catogries =
+    resMenu?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ==
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  return (
+    <div className="text-center">
+      <h1 className="font-semibold m-3 text-2xl">{name}</h1>
+      <p className="font-semibold text-lg">
+        {cuisines.join(", ")}:-
+        {costForTwoMessage}
+      </p>
+      {catogries.map((catogry, index) => (
+        <ResturantCatogry
+          key={++key}
+          data={catogry?.card?.card}
+          index={index}
+          showitems={resOpen === index}
+          setResOpen={(val) => setResOpen(val)}
+        />
+      ))}
+    </div>
+  );
+};
 export default ResturantMenu;
